58da529be4071ab4682a720da3c9c4e8eda97749
Pooja Kabir <pookabir95@gmail.com>
2018-05-02 17:55:45 +0530

    Final commit

--- src/PDFPassword.java
+++ src/PDFPassword.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import com.itextpdf.kernel.pdf.EncryptionConstants;
@@ -15,25 +16,34 @@ public class PDFPassword {
 	public static final String DEST = "hello_encrypted.pdf";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		try {
-			File file = new File(DEST);
+		//	File file = new File(DEST);
 			// file.getParentFile().mkdirs();
-			new PDFPassword().manipulatePdf(DEST);
+			new PDFPassword().manipulatePdf(SRC,DEST);
+		
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
 
-	public void manipulatePdf(String src) throws IOException {
+	public void manipulatePdf(String src,String dest) throws IOException {
+		File source = new File(src);
+		
+
 		PdfReader reader = new PdfReader(SRC, new ReaderProperties().setPassword("World".getBytes()));
 		PdfWriter writer = new PdfWriter(DEST,
 		new WriterProperties().setStandardEncryption("Hello".getBytes(), "World".getBytes(),
 						EncryptionConstants.ALLOW_PRINTING,
 						EncryptionConstants.ENCRYPTION_AES_128 | EncryptionConstants.DO_NOT_ENCRYPT_METADATA));
 		PdfDocument pdfDoc = new PdfDocument(reader, writer);
+		File destination = new File(dest);
+		source.delete();
+		destination.renameTo(new File(src));
+		
 		pdfDoc.close();
 	}
 }
+
+
